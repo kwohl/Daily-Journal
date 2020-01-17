ee9157e10b3f92eb5048ded217b2e54278c7d9e0
Kathryn Wohl <ktwohl@gmail.com>
2020-01-17 14:09:03 -0600

    added journal entries array and objects for several entries

---
+++ src/scripts/journal.js
@@ -0,0 +1,22 @@
+const journalEntry = {
+
+};
+
+const journalEntryHTML = {
+
+};
+
+const journalEntryCSS = {
+
+};
+
+const journalEntries = [
+
+];
+
+journalEntries.push(journalEntry);
+journalEntries.push(journalEntryHTML);
+journalEntries.push(journalEntryCSS);
+
+
+console.log("hello world");
