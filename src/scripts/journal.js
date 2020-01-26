2cb2b2ee2fd701cb63c88b12d2a7fc4749175539
Kathryn Wohl <ktwohl@gmail.com>
2020-01-26 13:29:41 -0600

    added json file and moved journal entries from js to json

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -5,27 +5,27 @@ const journalEntry = {
     mood: ""
 };
 
-const journalEntries = [
-    {
-        date: "20/01/20",
-        concept: "Building DOM Components",
-        entry: "We learned about adding HTML components through javascript. It mostly made sense,  but I still don't fully understand what the DOM is.",
-        mood: "Motivated"
-    },
-    {
-        date: "21/01/20",
-        concept: "Fetching Data & API",
-        entry: "I had a hard time focusing on this lesson and will have to go back and watch a lot of videos.",
-        mood: "Frustrated"
-    },
-    {
-        date: "22/01/20",
-        concept: "Events",
-        entry: "We learned about event listeners. I stayed in the room instead of going to the hackery and actually managed to get a lot done.",
-        mood: "Hopeful"
-    }
+// const journalEntries = [
+//     {
+//         date: "20/01/20",
+//         concept: "Building DOM Components",
+//         entry: "We learned about adding HTML components through javascript. It mostly made sense,  but I still don't fully understand what the DOM is.",
+//         mood: "Motivated"
+//     },
+//     {
+//         date: "21/01/20",
+//         concept: "Fetching Data & API",
+//         entry: "I had a hard time focusing on this lesson and will have to go back and watch a lot of videos.",
+//         mood: "Frustrated"
+//     },
+//     {
+//         date: "22/01/20",
+//         concept: "Events",
+//         entry: "We learned about event listeners. I stayed in the room instead of going to the hackery and actually managed to get a lot done.",
+//         mood: "Hopeful"
+//     }
 
-];
+// ];
 
 // journalEntries.push(journalEntry);
 
