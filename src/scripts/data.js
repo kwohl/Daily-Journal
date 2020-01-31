ad7be33c919c7b460a4d75897540637a3489455b
Kathryn Wohl <ktwohl@gmail.com>
2020-01-31 15:38:01 -0600

    fixed function calls to ensure all modules were communicating as needed, and removed type=module from script links

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -5,11 +5,18 @@
 //     mood: ""
 // };
 
-const getEntries = () => {
-fetch('http://localhost:8088/entries') // Fetch from the API
-    .then(resp => resp.json())  // Parse as JSON
-    .then(entries => {
-        renderJournalEntries(entries)
-        // What should happen when we finally have the array?
-        });
-};
+// const getEntries = () => {
+// fetch('http://localhost:8088/entries') // Fetch from the API
+//     .then(resp => resp.json())  // Parse as JSON
+//     .then(entries => {
+//         renderJournalEntries(entries)
+//         // What should happen when we finally have the array?
+//         });
+// };
+
+const api = {
+    getJournalEntries() {
+        return fetch("http://localhost:8088/entries")
+            .then(response => response.json())
+    }
+}
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,1 +1,9 @@
-getEntries();
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+*/
+
+api.getJournalEntries().then(renderJournalEntries);
